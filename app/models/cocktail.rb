f449133c40e72f41f8c0faccaca0bf5122dda60b
julien catonnet <julien.catonnet@essec.edu>
2020-12-18 12:17:51 +0100

    cocktaiiil features

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -3,5 +3,15 @@ class Cocktail < ApplicationRecord
     validates :name, presence: true, uniqueness: true
     has_many :doses, dependent: :destroy
     has_many :ingredients, through: :doses
+    has_one_attached :photo
+    validates :photo, presence: true
     
+    include PgSearch::Model
+
+    pg_search_scope :search_by_name_and_ingredient,
+      against: [ :name ],
+      using: {
+        tsearch: { prefix: true } 
+      }
+
 end
